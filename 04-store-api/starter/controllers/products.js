a48ffec9d8c52bee7a6cc676a0566a34c2192ec8
wjahoward <howardwang42@gmail.com>
2022-05-17 16:32:20 +0800

    added simple regex for search

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,16 +1,28 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find();
+    // const search = 'tm'; // consecutive 'tm' letters found
+    const search = 'a'; // search for letter 'a'
+    const products = await Product.find({
+        name: { $regex: search, $options: 'i' /* case insensitive */}
+    });
     return res.status(200).json({products});
 };
 
 const getAllProducts = async (req, res) => {
-    const { feature } = req.query; // filter criteria
+    const { feature, company, name } = req.query; // filter criteria
     const queryObject = {};
 
     if (feature) {
         queryObject.feature = feature === 'true' ? true : false;
+    }
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' /* case insensitive */};
     }
 
     const products = await Product.find(queryObject);
