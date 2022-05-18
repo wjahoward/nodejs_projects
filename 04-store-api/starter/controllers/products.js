ce216c25076abb6d4f004f7ffcd6cabfd319d042
wjahoward <howardwang42@gmail.com>
2022-05-18 11:55:06 +0800

    add pagination

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -41,8 +41,15 @@ const getAllProducts = async (req, res) => {
         result = result.select(fieldsList); // show only certain fields as prescribed by the user
     }
 
+    // pagination
+    // const page = Number(req.query.page) || 1
+    // const limit = Number(req.query.limit) || 10
+    // const skip = (page - 1) * limit;
+
+    // result = result.skip(skip).limit(limit);
+
     const products = await result;
-    return res.status(200).json(products);
+    return res.status(200).json(products);  
 };
 
 module.exports = {
