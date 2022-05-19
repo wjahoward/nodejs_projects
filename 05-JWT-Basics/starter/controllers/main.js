c060349fbc4a1d18614eb4cdb073ceb107783a33
wjahoward <howardwang42@gmail.com>
2022-05-19 15:05:26 +0800

    add comments

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -19,7 +19,7 @@ const login = async (req, res) => {
 
     // try to keep payload small, better experience for user
     // just for demo, in production use long, complex and unguessable string value
-    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'}); // do not put confidential information inside i.e. password  
+    const token = jwt.sign({id, username}, /* id and username are part of the payload */process.env.JWT_SECRET, {expiresIn: '30d'}); // do not put confidential information inside i.e. password  
 
     res.status(200).json({msg: 'user created', token});
 };
