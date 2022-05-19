98a975a9e63a4a2983575c9138f2a8dd12e15728
wjahoward <howardwang42@gmail.com>
2022-05-19 18:13:16 +0800

    another way of sending customised error resposnes rather than using mongoose validator

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,7 +1,17 @@
 const User = require('../models/User');
 const {StatusCodes} = require('http-status-codes');
+const {BadRequest, BadRequestError} = require('../errors');
 
 const register = async (req, res) => {
+    const {name, email, password} = req.body;
+
+    // NOTE: OPTIONAL
+    // this is the more meaningful way of sending validating responses
+    // as mongoose validator response can be too lengthy
+    if (!name || !email || !password) {
+        throw new BadRequestError('Please provide name, email and password');
+    }
+
     const user = await User.create(req.body);
     res.status(StatusCodes.CREATED).json(user);
 };
