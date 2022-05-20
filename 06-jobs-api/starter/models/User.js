413cdf3978a8c147c292e6e1a5399b8882496492
wjahoward <howardwang42@gmail.com>
2022-05-20 15:56:55 +0800

    created job schema

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
