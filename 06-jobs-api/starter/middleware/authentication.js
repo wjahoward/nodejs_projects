f398067ac854229fb3419f3cfad8736328570c4d
wjahoward <howardwang42@gmail.com>
2022-05-20 15:10:02 +0800

    alternative way of doing in authentication.js

--- 06-jobs-api/starter/middleware/authentication.js
+++ 06-jobs-api/starter/middleware/authentication.js
@@ -16,6 +16,11 @@ const auth = (req, res, next) => {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
         // attach the user to the job routes
         req.user = {userId: payload.userId, name: payload.name};
+
+        // alternative way of doing line 18
+        // const user = User.findById(payload.id).select('-password');
+        // req.user = user;
+
         next();
     } catch (err) {
         throw new UnauthenticatedError('Authentication invalid');
