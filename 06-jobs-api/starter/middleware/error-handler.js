fadbb633632ec56ede36d4bb8c54b45610c7b850
wjahoward <howardwang42@gmail.com>
2022-05-21 23:45:08 +0800

    validation error

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -8,13 +8,21 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong, try again later"
   };
   
-  if (err instanceof CustomAPIError) { // examples that will pass to here include logging in using invalid credentials
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
+  // by commenting the below codes, it can still be able to display the output
+  // if (err instanceof CustomAPIError) { // examples that will pass to here include logging in using invalid credentials
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
 
   if (err.code && err.code == 11000) {
     customError.statusCode = 400;
     customError.msg = `Email: ${err.keyValue.email} has already exist`;
+  }
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+                            .map((item) => item.message)
+                            .join(', ');
+    customError.statusCode = 400;
   }
 
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
