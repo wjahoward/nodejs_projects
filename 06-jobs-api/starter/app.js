99613a741674cfe2f681e9fd5d7ab4310fb4687a
wjahoward <howardwang42@gmail.com>
2022-05-22 00:22:26 +0800

    security setup

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -2,6 +2,13 @@
 
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -18,7 +25,16 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
+
 // extra packages
+app.set('trust proxy', 1); // enable if behind a reverse proxy (heroku, bluemix, AWS ELB, Nginx, etc)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+}));
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
