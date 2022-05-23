0051c5330114b554d609f34b2f716a4f39dea89d
wjahoward <howardwang42@gmail.com>
2022-05-23 14:49:14 +0800

    create basic routes and middleware and created User model

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,16 +1,30 @@
 require('dotenv').config()
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
+// rest of the packages
+const morgan = require('morgan');
+
+// errors
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddlware = require('./middleware/error-handler');
+
 const port = process.env.PORT || 3000;
+
+// connect to db
+const connectDB = require('./db/connect');
+
+app.use(morgan('tiny')); // allows one to knoow which routes have been accessed
+app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.send("hi world");
 });
 
-// connect to db
-const connectDB = require('./db/connect');
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddlware);
 
 const start = async () => {
     try {
