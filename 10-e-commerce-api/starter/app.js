4ffb7bd5fad4e607093e8bef4e2cff74d1e71b8c
wjahoward <howardwang42@gmail.com>
2022-05-23 12:19:05 +0800

    setup basic express server

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,20 @@
-console.log('E-Commerce API');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    return res.send("hi world");
+});
+
+const start = async () => {
+    try {
+        await app.listen(port, () => {
+            console.log(`app is listening on port number ${port}`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+start();
