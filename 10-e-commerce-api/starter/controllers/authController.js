59b48c5f4f4244ff3945c0ca815e2d704a7052ef
wjahoward <howardwang42@gmail.com>
2022-05-23 16:10:36 +0800

    validation for email unique

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -1,5 +1,29 @@
+const User = require('../models/User');
+const {StatusCodes} = require('http-status-codes');
+const {CustomError, BadRequestError} = require('../errors');
+
 const register = async (req, res) => {
-    return res.send('register user');
+    const {
+        name,
+        email,
+        password
+    } = req.body;
+
+    // if any of the inputs are left empty
+    if (!name || !email || !password) {
+        throw new BadRequestError("Please input name, email and password");
+    }
+
+    // verify email unique
+    const emailAlreadyExists = await User.findOne({email});
+
+    if (emailAlreadyExists) {
+        throw new BadRequestError('Email already exists');
+    }
+
+    const user = User.create(req.body);
+    return res.status(StatusCodes.CREATED).json(user);
+
 };
 
 const login = async (req, res) => {
--- 10-e-commerce-api/starter/models/User.js
+++ 10-e-commerce-api/starter/models/User.js
@@ -10,6 +10,7 @@ const UserSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: [true, "Please input an email"],
         validate: {
             validator: validator.isEmail,
