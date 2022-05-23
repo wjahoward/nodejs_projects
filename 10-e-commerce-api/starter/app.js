068dba81b162e25b0b770634d0a6d87b3285b1e1
wjahoward <howardwang42@gmail.com>
2022-05-23 12:22:09 +0800

    connect to db

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 
@@ -7,9 +9,13 @@ app.get('/', (req, res) => {
     return res.send("hi world");
 });
 
+// connect to db
+const connectDB = require('./db/connect');
+
 const start = async () => {
     try {
-        await app.listen(port, () => {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
             console.log(`app is listening on port number ${port}`);
         });
     } catch (err) {
