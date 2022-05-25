f0c0a8fcbc37c819091875865b901dfabe0bdebf
wjahoward <howardwang42@gmail.com>
2022-05-25 23:09:33 +0800

    done CRUD for product controllers

--- 10-e-commerce-api/starter/controllers/productController.js
+++ 10-e-commerce-api/starter/controllers/productController.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const Product = require("../models/Product");
+const CustomErr = require("../errors");
 
 const createProduct = async (req, res) => {
     req.body.user = req.user.userId;
@@ -8,19 +9,51 @@ const createProduct = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-    res.send("get all products");
+    const products = await Product.find();
+    res.status(StatusCodes.OK).json(products);
 };
 
 const getSingleProduct = async (req, res) => {
-    res.send("get one product");
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    
+    if (!product) {
+        throw new CustomErr.NotFoundError(`No such product of id ${id} is found`);
+    }
+
+    res.status(StatusCodes.OK).json(product);
 };
 
 const updateProduct = async (req, res) => {
-    res.send("update product");
+    const { id } = req.params;
+
+    const product = await Product.findOneAndUpdate({
+        _id: id},
+        req.body,
+        {
+            new: true,
+            runValidators: true
+        }
+    );
+
+    if (!product) {
+        throw new CustomErr.NotFoundError(`No such product of id ${id} is found`);
+    }
+
+    res.status(StatusCodes.OK).json(product);
+    
 };
 
 const deleteProduct = async (req, res) => {
-    res.send("delete product");
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    
+    if (!product) {
+        throw new CustomErr.NotFoundError(`No such product of id ${id} is found`);
+    }
+
+    await product.remove();
+    res.status(StatusCodes.OK).json({msg: "Product has been removed"});
 };
 
 const uploadImage = async (req, res) => {
