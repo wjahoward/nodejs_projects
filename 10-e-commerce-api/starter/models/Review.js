52f6027c2d19d421c4d676e502c053d03ef0899b
wjahoward <howardwang42@gmail.com>
2022-05-28 23:42:59 +0800

    trigger keys for average rating

--- 10-e-commerce-api/starter/models/Review.js
+++ 10-e-commerce-api/starter/models/Review.js
@@ -37,4 +37,34 @@ const ReviewSchema = new mongoose.Schema({
 // 1 review for 1 product
 ReviewSchema.index({ user: 1, product: 1 }, { unique: true });
 
+ReviewSchema.statics.calculateAverageRating = async function (productId) {
+    const result = await this.aggregate([
+        {$match:{product:productId}},
+        {$group:{
+            _id:null,
+            averageRating:{ $avg:'$rating' },
+            numOfReviews: { $sum: 1 }
+        }}
+    ]);
+    try {
+        await this.model('Product').findOneAndUpdate(
+            { _id: productId },
+            {
+                averageRating: Math.ceil(result[0]?.averageRating || 0),
+                numOfReviews: result[0]?.numOfReviews || 0
+,            }
+        );
+    } catch (err) {
+        console.log(err); 
+    }
+};
+
+ReviewSchema.post('save', async function() {
+    await this.constructor.calculateAverageRating(this.product); 
+});
+
+ReviewSchema.post('remove', async function () {
+    await this.constructor.calculateAverageRating(this.product);
+})
+
 module.exports = mongoose.model('Review', ReviewSchema);
