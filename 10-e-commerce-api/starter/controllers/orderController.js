b5f71fdd61dbce1cc458c6323c2d8206a17b242e
wjahoward <howardwang42@gmail.com>
2022-05-29 15:09:52 +0800

    doen order controllers

--- 10-e-commerce-api/starter/controllers/orderController.js
+++ 10-e-commerce-api/starter/controllers/orderController.js
@@ -1,21 +1,112 @@
+const Product = require('../models/Product');
+
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { checkPermission } = require('../utils');
+const Order = require('../models/Order');
+
+const fakeStripeAPI = async ({amount, currency}) => {
+    const client_secret = 'someRandomValue';
+    return {client_secret, amount};
+};
+
 const getAllOrders = async (req, res) => {
-    res.send("get all orders");
+    const orders = await Order.find();
+    res.status(StatusCodes.OK).json(orders);
 };
 
 const getSingleOrder = async (req, res) => {
-    res.send("get single order");
+    const { id } = req.params;
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+        throw new CustomError.NotFoundError(`No such order with id ${id}`);
+    }
+
+    checkPermission(req.user, order.user);
+    res.status(StatusCodes.OK).json(order);
 };
 
 const getCurrentUserOrders = async (req, res) => {
-    res.send("get current user order");
+    const orders = await Order.find({
+        user: req.user.userId
+    });
+    res.status(StatusCodes.OK).json({ orders, count: orders.length });
 };
 
 const createOrder = async (req, res) => {
-    res.send("create an order");
+    const { tax, shippingFees, items } = req.body;
+    let orderItems = [];
+    let subtotal = 0;
+
+    if (!items || items.length < 1) {
+        throw new CustomError.BadRequestError('No cart items provided');
+    }
+
+    if (!tax || !shippingFees) {
+        throw new CustomError.BadRequestError('Please provide tax and shipping fee');
+    }
+
+    for (const item of items) {
+        const dbProductId = item.product;
+        const dbProduct = await Product.findById(dbProductId);
+
+        if (!dbProduct) {
+            throw new CustomError.NotFoundError(`No such product of id ${dbProductId} is found`);
+        }
+
+        const {name, price, image, _id} = dbProduct;
+        const singleOrderItem = {
+            amount: item.amount,
+            name,
+            price,
+            image,
+            product: _id
+        }
+
+        // add item to order
+        orderItems = [...orderItems, singleOrderItem];
+
+        // calculate subtotal
+        subtotal += item.amount * price;
+    }
+
+    const total = tax + shippingFees + subtotal;
+    // get client secret
+    const paymentIntent = await fakeStripeAPI({
+        amount: total,
+        currency: 'usd'
+    });
+
+    const order = await Order.create({
+        orderItems, 
+        total,
+        subtotal,
+        tax,
+        shippingFees,
+        clientSecret: paymentIntent.client_secret,
+        user: req.user.userId
+    });
+
+    res.status(StatusCodes.CREATED).json({order, clientSecret: order.clientSecret});
 };
 
 const updateOrder = async (req, res) => {
-    res.send("update an order");
+    const { id } = req.params;
+    const { paymentIntentId } = req.body;
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+        throw new CustomError.NotFoundError(`No order with id ${id}`);
+    }
+
+    checkPermission(req.user, order.user);
+
+    order.paymentIntentId = paymentIntentId;
+    order.status = 'paid';
+    await order.save();
 };
 
 module.exports = {
--- 10-e-commerce-api/starter/models/Order.js
+++ 10-e-commerce-api/starter/models/Order.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const SingleCartItemSchema = mongoose.Schema({
+const SingleOrderItemSchema = mongoose.Schema({
     name: { type: String, required: true },
     image: { type: String, required: true },
     price: { type: Number, required: true },
@@ -29,7 +29,7 @@ const OrderSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
-    cartItems: [SingleCartItemSchema],
+    cartItems: [SingleOrderItemSchema],
     status: {
         type: String,
         enum: ['pending', 'failed', 'paid', 'delivered', 'cancelled'],
