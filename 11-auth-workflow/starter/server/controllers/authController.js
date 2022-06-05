e1cfad194169e24ec86f3ad753d4383fc32e1c8f
wjahoward <howardwang42@gmail.com>
2022-06-05 11:15:48 +0800

    refactor register to not allow user to automatically login upon register, the user need to verify email first in order to login

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -15,10 +15,19 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
-  const user = await User.create({ name, email, password, role });
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  const verificationToken = "fake token";
+
+  const user = await User.create({ name, email, password, role, verificationToken });
+
+  // const tokenUser = createTokenUser(user);
+  // attachCookiesToResponse({ res, user: tokenUser });
+  res
+    .status(StatusCodes.CREATED)
+    .json(
+      { 
+        msg: "Success! Please check your email to verify account",
+        verificationToken: verificationToken
+      });
 };
 const login = async (req, res) => {
   const { email, password } = req.body;
